fc960ac2ef3a7f906d7349a2f53485c3663c260a
Kadri Sylejmani <kadri.sylejmani@uni-pr.edu>
2020-08-24 22:48:10 +0200

    Update sdr_algorithm.cpp

--- Self-driving rides/sdr_algorithm.cpp
+++ Self-driving rides/sdr_algorithm.cpp
@@ -12,6 +12,7 @@
 #include <iomanip>
 #include <math.h>
 #include <stdexcept> // std::runtime_error
+#include <list>
 using namespace std;
 
 //Parameters
@@ -20,7 +21,7 @@ const int no_close_next_rides = 3;
 const int no_replace_next_rides = 3;
 const int difference_earliest_start_time = 5;
 const int difference_latest_finish_time = 5;
-string instance_name = "a_example";
+string instance_name = "b_should_be_easy";
 string instance_path = "dataset/" + instance_name + ".in";
 string solution_path = "solution/" + instance_name + ".out";
 
@@ -136,6 +137,7 @@ struct data_set {
 	}
 };
 
+map<int, vector<int>> get_initial_solution_variant_1(data_set ds);
 struct submission
 {
 	map<int, vector<int>> fleets;
@@ -274,8 +276,9 @@ struct submission
 		// Save the solution of fleets to the file with the name of the variable solution_path
 		// Test the functionality of the code in the main function
 	}
-	void show_assigned_roads() {
+	void show_assigned_roads(data_set ds) {
 		ofstream csv_file(solution_path);
+		map<int, vector<int>> fleets = get_initial_solution_variant_1(ds) ;
 		for (map<int, vector<int>>::iterator itr = fleets.begin(); itr != fleets.end(); itr++)
 		{
 			for (int j = 0; j < itr->second.size(); j++)
@@ -370,12 +373,89 @@ int get_score(data_set ds, map<int, vector<int>> fleets) {// To be done by Erlis
 }
 
 map<int, vector<int>> get_initial_solution_variant_1(data_set ds) {
-	// Variant 1 - Fully random, to be done by Enes
-	map<int, vector<int>> result;
-	//Implementation goes here ...
-	//Test your solution in the main function
-	return result;
-}
+		// Variant 1 - Fully random, to be done by Enes
+		map<int, vector<int>> result;
+		vector<int>* vehicles = new vector<int>[ds.F];
+		srand(time(NULL));
+		list<int> assigned_rides;
+		//counters count the number of rides we assign to a car
+		vector <int> Counters(ds.F, 0);
+		//Implementation goes here ...
+		for (int i = 0; i < ds.F; i++)
+		{
+			int time_left = ds.T;
+			//list of the rides that are unaffordable for the car i
+			list <int> unaffordable_rides;
+			//while there is some time left, do ...
+			while (time_left != 0)
+			{
+			label:
+				int random_ride = rand() % ds.N;
+				for (auto it = assigned_rides.begin(); it != assigned_rides.end(); it++)
+					if (*it == random_ride)
+						goto label;
+				for (auto itr = unaffordable_rides.begin(); itr != unaffordable_rides.end(); itr++)
+					if (*itr == random_ride)
+						goto label;
+
+				//earliest_time is the number of steps that has passed by the time vehicle starts riding at the starting intersection , we firstly say that it is equal to the earliest start
+				int earliest_time = ds.rides.at(random_ride)->s;
+
+				//if this is the first ride for vehicle i
+				if (time_left == ds.T) {
+					if (ds.rides.at(random_ride)->s < (ds.rides.at(random_ride)->a + ds.rides.at(random_ride)->b))
+						earliest_time = ds.rides.at(random_ride)->a + ds.rides.at(random_ride)->b;
+				}
+				//if this isn't the first ride for vehicle i
+				else
+				{
+					//distance_last_first is the distance from the last intersection of the previous ride to the begining of the start intersection of the current ride
+					int distance_last_first = abs(ds.rides.at(random_ride)->a - ds.rides.at(assigned_rides.back())->x) + abs(ds.rides.at(random_ride)->b - ds.rides.at(assigned_rides.back())->y);
+					if (ds.rides.at(random_ride)->s < ds.T - time_left + distance_last_first)
+						earliest_time = ds.T - time_left + distance_last_first;
+				}
+
+				int distance_between_intersections = abs(ds.rides.at(random_ride)->a - ds.rides.at(random_ride)->x) + abs(ds.rides.at(random_ride)->b - ds.rides.at(random_ride)->y);
+				//if the ride ended before or in the latest step , do ...
+				if (earliest_time + distance_between_intersections <= ds.rides.at(random_ride)->f)
+				{
+					assigned_rides.push_back(random_ride);
+					vehicles[i].push_back(random_ride);
+					time_left = ds.T - (earliest_time + distance_between_intersections);
+					Counters.at(i)++;
+					//if all rides are assigned or if we have checked all rides for the car i there is nothing left to check so go to the next car (break) 
+					if ( (assigned_rides.size() == ds.N) || (unaffordable_rides.size() + assigned_rides.size() == ds.N))
+						break;				
+				}
+				//if the ride didn't end before or in the last step (it's unaffordable for car i) , do ...
+				else
+				{
+					unaffordable_rides.push_back(random_ride);
+					//if all rides are unaffordable (pretty sure that this condition never gets true , put it for the sake of safeness) , or we have checked all rides , break .
+					if ((unaffordable_rides.size() == ds.N - assigned_rides.size()) || (unaffordable_rides.size() == ds.N))
+						break;
+				}
+				//Test your solution in the main function
+
+			}
+			vehicles[i].insert(vehicles[i].begin(), Counters.at(i));
+			result.insert(pair<int, vector<int>>(i, vehicles[i]));
+			//if all rides are assigned for the cars up to i , do
+			if (assigned_rides.size() == ds.N) {
+				//if this isn't the last car left
+				if (i != ds.F - 1)
+				{
+					//assign the rest of the cars 0 rides
+					for (int j = i + 1; j < ds.F; j++) {
+						vehicles[j].push_back(0);
+						result.insert(pair<int, vector<int>>(j, vehicles[j]));
+					}
+				}
+				break;
+			}
+		}
+		return result;
+	}
 
 map<int, vector<int>> get_initial_solution_variant_2(data_set ds) {
 	// Variant 2 - Smart random, to be done by Lendrit
@@ -419,7 +499,7 @@ int main() {
 	}
 	//Test of submission structure
 	submission s;
-	s.show_assigned_roads();
+	s.show_assigned_roads(d1);
 	
 	// Test get_score function
 	data_set ds;
@@ -436,5 +516,11 @@ int main() {
 	int score = get_score(ds, fleets);
 	cout << "Score: " << score << endl;
 	
+	// Test get_initial_solution_variant1
+	fleets= get_initial_solution_variant_1(ds);
+	int score1 = get_score(ds, fleets);
+	cout << "Score1: " << score1 << endl;
+	
 	return 0;
 }
+
