b463e0bf536f6632f135d08cfa3234b38a291212
EnesHasani <66223798+EnesHasani@users.noreply.github.com>
2020-09-23 20:08:33 +0200

    Update some_functions.cpp

--- Self-driving rides/src/some_functions.cpp
+++ Self-driving rides/src/some_functions.cpp
@@ -227,31 +227,31 @@ int get_score_for_one_vehicle(const data_set& ds,const vector<int>& scoring_vehi
 		int distance_from_zero_intersection = ds.rides.at(scoring_vehicle[1])->a + ds.rides.at(scoring_vehicle[1])->b;
 		for (int i = 1; i < scoring_vehicle.size(); i++)
 		{
-			int distance_between_intersections = 0;
-			int distance_last_first = 0;
-			distance_between_intersections = abs(ds.rides.at(scoring_vehicle[i])->a - ds.rides.at(scoring_vehicle[i])->x) + abs(ds.rides.at(scoring_vehicle[i])->b - ds.rides.at(scoring_vehicle[i])->y);
+			int distance_between_intersections = abs(ds.rides.at(scoring_vehicle[i])->a - ds.rides.at(scoring_vehicle[i])->x) + abs(ds.rides.at(scoring_vehicle[i])->b - ds.rides.at(scoring_vehicle[i])->y);
 			if (i == 1)
 			{
-				score += distance_between_intersections;
 				if (ds.rides.at(scoring_vehicle[i])->s >= distance_from_zero_intersection) {
-					time_passed = ds.rides.at(scoring_vehicle[i])->s;
-					score += ds.B;
-					time_passed += distance_between_intersections;
+					time_passed = ds.rides.at(scoring_vehicle[i])->s + distance_between_intersections;
+					score = ds.B + distance_between_intersections;
 					continue;
 				}
-				time_passed += distance_between_intersections+ distance_from_zero_intersection;
+				time_passed = distance_between_intersections + distance_from_zero_intersection;
+				if (time_passed > ds.rides.at(scoring_vehicle[i])->f)
+					continue;
+				score = distance_between_intersections;
 				continue;
 			}
-			distance_last_first = abs(ds.rides.at(scoring_vehicle[i])->a - ds.rides.at(scoring_vehicle[i - 1])->x) + abs(ds.rides.at(scoring_vehicle[i])->b - ds.rides.at(scoring_vehicle[i - 1])->y);
+			int distance_last_first = abs(ds.rides.at(scoring_vehicle[i])->a - ds.rides.at(scoring_vehicle[i - 1])->x) + abs(ds.rides.at(scoring_vehicle[i])->b - ds.rides.at(scoring_vehicle[i - 1])->y);
 			time_passed += distance_last_first ; 
-			score += distance_between_intersections; 
 			if (ds.rides.at(scoring_vehicle[i])->s >= time_passed) {
-				time_passed = ds.rides.at(scoring_vehicle[i])->s;
-				time_passed += distance_between_intersections;
-				score += ds.B;
+				time_passed = ds.rides.at(scoring_vehicle[i])->s + distance_between_intersections;
+				score += ds.B + distance_between_intersections;
 				continue;
 			}
 			time_passed += distance_between_intersections;
+			if (time_passed > ds.rides.at(scoring_vehicle[i])->f)
+				continue;
+			score += distance_between_intersections ;
 		}
 		return score;
 	}
