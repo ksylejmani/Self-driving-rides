50c2d734b00ce4fb5cf28f9f9c629fcb4ad843c8
LendritIbrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2020-10-02 07:21:44 +0200

    Revert "Added close replacing rides"
    This reverts commit 5ec02eb67bebae147b5afdd757e86f87cc54b933.

--- Self-driving rides/include/data_set_h.h
+++ Self-driving rides/include/data_set_h.h
@@ -10,27 +10,25 @@ struct ride;
 
 using namespace std;
 
-struct data_set
-{
-	int R, C, F, N, B, T;
-	vector<ride *> rides;
-	string instance;
-
+struct data_set {
+	int 			R, C, F, N, B, T;
+	vector<ride*>	rides;
+	string 			instance;
+	
 	data_set();
 	data_set(string);
 
-	void read_instance_file();
-	void process_first_line(string);
-	void process_ride_lines(string, int);
+	void 			read_instance_file();
+	void 			process_first_line(string);
+	void 			process_ride_lines(string, int);
 
 	//Could be private
-	ride *find_close_ride(ride *, bool);
+	ride* 			find_close_ride(ride*);
 
-	void find_k_close_replace(ride *);
-	void find_k_close_next(ride *);
+	void 			find_k_closest_rides(ride*);
 
-	int get_closest_distance(ride *, ride *, bool);
-	void get_instance_details(vector<int> &);
+	int 			get_closest_distance(ride*, ride*);
+	void 			get_instance_details(vector<int>&);
 };
 
 #endif
